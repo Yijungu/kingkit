bdcb4fc473a0646a0f7b2c5ccacebe80948be521
Yijungu <wnsrn4970@naver.com>
2025-06-18 15:52:22 +0900

    [refactor] EC2 보안그룹 규칙 재정비 및 불필요한 ingress 정리

--- infra/terraform/modules/iam/attach.tf
+++ infra/terraform/modules/iam/attach.tf
@@ -1,14 +1,17 @@
 resource "aws_iam_user_policy_attachment" "user_attach_state" {
   user       = "terraform-github-actions"
   policy_arn = aws_iam_policy.terraform_state.arn
+  depends_on = [aws_iam_policy_version.terraform_state_v]
 }
 
 resource "aws_iam_user_policy_attachment" "user_attach_ec2" {
   user       = "terraform-github-actions"
   policy_arn = aws_iam_policy.ec2_control.arn
+  depends_on = [aws_iam_policy_version.ec2_control_v]
 }
 
 resource "aws_iam_user_policy_attachment" "user_attach_iam_rds" {
   user       = "terraform-github-actions"
   policy_arn = aws_iam_policy.iam_rds.arn
+  depends_on = [aws_iam_policy_version.iam_rds_v]
 }
