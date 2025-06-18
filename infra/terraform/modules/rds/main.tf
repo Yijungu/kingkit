5d854b943e35973641454cbfeb19780d6bdc819c
Yijungu <wnsrn4970@naver.com>
2025-06-18 19:14:53 +0900

    [refactor] EC2 보안그룹 규칙 재정비 및 불필요한 ingress 정리

--- infra/terraform/modules/rds/main.tf
+++ infra/terraform/modules/rds/main.tf
@@ -10,6 +10,6 @@ resource "aws_db_instance" "this" {
   publicly_accessible     = var.publicly_accessible
   skip_final_snapshot     = true
   deletion_protection     = false
-  vpc_security_group_ids  = [var.security_group_id]
+  vpc_security_group_ids = var.security_group_ids
   backup_retention_period = var.backup_retention_period
 }
